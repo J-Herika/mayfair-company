cbc33484564589634e7d16987a9d77ff4b452de0
J-Herika <138695574+J-Herika@users.noreply.github.com>
2025-05-14 22:45:27 +0300

    finish my page design only

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,3 +2,128 @@
 	import { gsap } from 'gsap';
 </script>
 
+<header class="font-grotesque mx-auto mt-5 flex w-7xl items-center justify-between">
+	<section>
+		<p class="text-4xl font-black">MayFair</p>
+		<p class="text-sm font-black">Built on Quality. Backed by 40 Years.</p>
+	</section>
+	<section class="flex gap-10 text-2xl font-extralight">
+		<p>About</p>
+		<p>Projects</p>
+		<p>Contact</p>
+	</section>
+</header>
+
+<main class="font-grotesque mt-10">
+	<div class="mx-auto w-7xl">
+		<img src="/Hero-Img.webp" alt="A black building designed like a Jenga" />
+		<section class="mt-10">
+			<h1 class="text-8xl font-black">MayFair Group</h1>
+			<div class="max-w-[80ch]">
+				<p class="mt-2.5 text-2xl font-light">
+					Established in 1984, we bring 40 years of proven expertise in civil works, MEP systems,
+					and critical infrastructure—built to perform, built to last.
+				</p>
+			</div>
+		</section>
+	</div>
+	<section class="font-grotesque mt-60">
+		<div class="mx-auto w-7xl"><h2 class="text-6xl font-black">What we Specialize in</h2></div>
+		<div class="mt-5">
+			<div class="border-primary flex h-[266px] items-center gap-10 border-t-2">
+				<p class="text-5xl">●</p>
+				<h3 class="text-7xl font-extralight">MEP Services</h3>
+			</div>
+			<div class="border-primary flex h-[266px] items-center gap-10 border-t-2">
+				<p class="text-5xl">●</p>
+				<h3 class="text-7xl font-extralight">Civil Works</h3>
+			</div>
+			<div class="border-primary flex h-[266px] items-center gap-10 border-t-2">
+				<p class="text-5xl">●</p>
+				<h3 class="text-7xl font-extralight">Pneumatic Tube System Projects</h3>
+			</div>
+			<div class="border-primary flex h-[266px] items-center gap-10 border-y-2">
+				<p class="text-5xl">●</p>
+				<h3 class="text-7xl font-extralight">Maintenance Services</h3>
+			</div>
+		</div>
+	</section>
+	<section class="font-grotesque mt-40">
+		<div class="mx-auto w-7xl"><h2 class="text-6xl font-black">Our Work</h2></div>
+		<div class="mt-5 flex h-[826px] items-center justify-between border-y-2">
+			<div class="flex flex-1 items-center justify-center text-5xl font-extralight">
+				<p>Civil</p>
+			</div>
+
+			<div class="h-full w-px bg-black"></div>
+
+			<div class="flex flex-1 items-center justify-center text-5xl font-extralight">
+				<p>Decoration</p>
+			</div>
+
+			<div class="h-full w-px bg-black"></div>
+
+			<div class="flex flex-1 items-center justify-center text-center text-5xl font-extralight">
+				<p>Pneumatic <br /> Tube System</p>
+			</div>
+
+			<div class="h-full w-px bg-black"></div>
+
+			<div class="flex flex-1 items-center justify-center text-5xl font-extralight">
+				<p>A/C</p>
+			</div>
+
+			<div class="h-full w-px bg-black"></div>
+
+			<div class="flex flex-1 items-center justify-center text-5xl font-extralight">
+				<p>MEP</p>
+			</div>
+		</div>
+	</section>
+	<section class="font-grotesque mx-auto mt-40 w-7xl">
+		<h2 class="text-6xl font-black">About Us</h2>
+		<div class="max-w-[80ch]">
+			<p class=" font-montserrat mt-6 text-xl font-normal">
+				Mayfair Building Contracting, established in 1984, is a recognized leader in the
+				Electro-Mechanical field—specializing in Civil Construction, MEP services, and Pneumatic
+				Tube Systems. Backed by a highly experienced team of engineers, supervisors, and
+				technicians, we are equipped to meet diverse client requirements and can rapidly mobilize
+				resources to any project site.
+			</p>
+		</div>
+		<button class="text-primary mt-12 border-4 px-6 py-5">Read More</button>
+	</section>
+</main>
+<footer class="bg-footer font-grotesque mt-40">
+	<div class=" mx-auto flex w-7xl items-center justify-between py-10">
+		<section class="w-[24ch]">
+			<p class="text-4xl font-black">MayFair</p>
+			<p class="text-2xl font-extralight">Legacy Built on Quality, Since 1984.</p>
+		</section>
+		<section class="flex gap-10 text-2xl font-extralight">
+			<p>About</p>
+			<p>Projects</p>
+			<p>Contact</p>
+		</section>
+		<section class="flex flex-col gap-3.5">
+			<p class="text-3xl font-black">Contact</p>
+			<p class="text-xl font-black">
+				Phone Number :
+				<span class="font-montserrat text-xl font-light">(+971) - 507670657</span>
+			</p>
+
+			<iframe
+				src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d331.8415044230076!2d-88.53732798262237!3d30.443398996175063!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x889bebc27560cf7d%3A0xda9c6e63fccb006a!2s4405%20Neese%20Ln%2C%20Moss%20Point%2C%20MS%2039563%2C%20USA!5e1!3m2!1sen!2str!4v1747248192466!5m2!1sen!2str"
+				width="260"
+				height="260"
+				style="border:0;"
+				allowfullscreen=""
+				loading="lazy"
+				referrerpolicy="no-referrer-when-downgrade"
+			></iframe>
+		</section>
+	</div>
+	<p class="font-montserrat pt-4 pb-2.5 text-center text-sm font-extralight">
+		© 2025 Mayfair Building Contracting LLC. All rights reserved.
+	</p>
+</footer>
